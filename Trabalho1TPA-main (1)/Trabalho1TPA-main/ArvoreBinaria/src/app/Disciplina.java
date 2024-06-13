7e8ac7477ceec5100a5a92d2fab0796690889035
Thalison Vinicius <69252647+Thalison17@users.noreply.github.com>
2024-06-13 09:46:43 -0300

    Update Disciplina.java

--- Trabalho1TPA-main (1)/Trabalho1TPA-main/ArvoreBinaria/src/app/Disciplina.java
+++ Trabalho1TPA-main (1)/Trabalho1TPA-main/ArvoreBinaria/src/app/Disciplina.java
@@ -8,34 +8,25 @@ public class Disciplina {
     private int cargaHoraria;
     private ArrayList<Disciplina> preRequisitos;
 
-    public Disciplina(int codigo, String nome, int cargaHoraria){
+    public Disciplina(int codigo, String nome, int cargaHoraria) {
         this.codigo = codigo;
         this.nome = nome;
         this.cargaHoraria = cargaHoraria;
         this.preRequisitos = new ArrayList<Disciplina>();
     }
 
-    public int getCodigo() {
-        return codigo;
-    }
+    public int getCodigo() { return codigo; }
 
-    public String getNome() {
-        return nome;
-    }
+    public String getNome() { return nome; }
 
-    public int getCargaHoraria() {
-        return cargaHoraria;
-    }
+    public int getCargaHoraria() { return cargaHoraria; }
 
-    public ArrayList<Disciplina> getPreRequisitos() {
-        return preRequisitos;
-    }
+    public ArrayList<Disciplina> getPreRequisitos() { return preRequisitos; }
 
-    public void addPreRequisito(Disciplina disciplina) {
-        preRequisitos.add(disciplina);
-    }
+    public void addPreRequisito(Disciplina disciplina) { preRequisitos.add(disciplina); }
 
+    @Override
     public String toString() {
-        return ("Código " + this.getCodigo() + ": " + this.getNome() + " - " + this.getCargaHoraria() + "h");
+        return "Código " + this.getCodigo() + ": " + this.getNome() + " - " + this.getCargaHoraria() + "h";
     }
 }
